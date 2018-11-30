5ad72626dc61328c633ce60e25750fd28a2a07dc
Amir Rachum <amir@rachum.com>
2018-11-30 21:30:17 +0200

    Bump version: 1.0.4rc → 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-version = '1.0.4rc'
+version = '1.0.4'
 
 with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     long_description = readme.read()
--- yieldfrom.py
+++ yieldfrom.py
@@ -180,7 +180,7 @@ handling of empty generators, etc.
 
 """
 
-__version__ = '1.0.4rc'
+__version__ = '1.0.4'
 __all__ = ('yieldfrom', 'From', 'Return')
 
 
